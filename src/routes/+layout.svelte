5861d3babf883a813bef96398b074a9300e97823
Cor Zuurmond <jczuurmond@pm.me>
2025-11-09 13:52:51 +0100

    Add background image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,24 @@
-<script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
 
+<script lang="ts">
 	let { children } = $props();
 </script>
 
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
+<div class="background-wrapper">
+	{@render children()}
+</div>
 
-{@render children()}
+<style>
+	:global(html), :global(body) {
+		margin: 0;
+		padding: 0;
+		height: 100%;
+	}
+
+	.background-wrapper {
+		min-height: 100vh;
+		background-image: url('/background.jpg');
+		background-size: cover;
+		background-position: center;
+		background-repeat: no-repeat;
+	}
+</style>
